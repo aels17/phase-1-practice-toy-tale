498dcbd378c1719f49bec2ba5e6400b0b75e048e
Ayman <aelsayed.nh@gmail.com>
2022-02-08 17:24:02 -0500

    commit tree 93a5eb078207fa9a6bd2df1f8c826726f56f08a0

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
 let addToy = false;
 
-document.addEventListener("DOMContentLoaded", () => {
+fetchAll()
+
+function fetchAll(){
+  fetch("http://localhost:3000/toys")
+  .then (res => res.json())
+  .then(objArray => {
+    objArray.forEach(obj => { 
+      objFunc(obj)
+    })
+  })
+}
+
+const toyContainer = document.getElementById("toy-collection");
+
+function objFunc(obj){
+  const card = document.createElement("div");
+  card.className = "poke-card";
+  
+  const displayImg = document.createElement("img");
+  displayImg.src = obj.image;
+  const ptag = document.createElement("p")
+  ptag.textContent = "Likes: " + obj.likes;
+
+  const btn = document.createElement("button");
+  btn.className = "like-btn"
+  btn.id = obj.id
+
+
+  card.append(displayImg, ptag, btn)
+
+  toyContainer.appendChild(card);
+
+  }
+
   const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
+
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
+ 
       toyFormContainer.style.display = "block";
     } else {
       toyFormContainer.style.display = "none";
     }
   });
-});
